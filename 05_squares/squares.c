a6edee45e510d4b3549e589536a4338cf5e3d01d
Coursera Learner <nobody@nowhere.nul>
2020-09-13 05:14:46 +0000

    Did assignment 5

--- 05_squares/squares.c
+++ 05_squares/squares.c
@@ -9,6 +9,10 @@
 int isInRange(int coord, int offset, int size) {
   // if coord is in range, return 1
   // else, return 0
+  if ((coord >= offset) && (coord < (offset + size)))
+    {
+      return 1;
+    }
   return 0;
 }
 
@@ -19,16 +23,50 @@ int isInRange(int coord, int offset, int size) {
 int isAtBorder(int coord, int offset, int size) {
   // if coord is equal to offest or offset + size
   // return 1, else return 0
+  if ((coord == offset) || (coord == (offset + size)))
+    {
+      return 1;
+    }
   return 0;
+}
+
+int max(int num1, int num2){
+  if (num1 > num2){
+    return num1;
+  }
+  else{
+    return num2;
+  }
 }
 
 void squares(int size1, int x_offset, int y_offset, int size2) {
   //compute the max of size1 and (x_offset + size2).  Call this w
-
+  int w = max(size1, (x_offset + size2));
   //compute the max of size1 and (y_offset + size2).  Call this h
-
+  int h = max(size1, (y_offset + size2));
   //count from 0 to h. Call the number you count with y
-
+  for (int y=0; y<=h; y++)
+    {
+      for (int x=0; x<=w; x++)
+	{
+	  if ((isInRange(x, x_offset, size2) && isAtBorder(y,y_offset,size2-1)) || (isInRange(y,y_offset,size2) && (isAtBorrder(x,x_offset,size2-1))))
+	    {
+	      printf("*");
+	    }
+	  else{
+	    if ((x<size1 && (y==0 || y==size1-1)) || ((y<size1) && (x==0 ||x=size1-1)))
+	      {
+		printf("#");
+	      }
+	    else
+	      {
+		printf(" ");
+	      }
+	  }
+	}
+      printf("\n");
+      
+    }
     //count from 0 to w. Call the number you count with x
 
       //check if  EITHER
@@ -51,3 +89,4 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
     //print a newline
 
 }
+
