1e35baa435e1379408f588c52c3f4fc28dd91ea3
Coursera Learner <nobody@nowhere.nul>
2020-09-13 05:18:24 +0000

    Did assignment 5

--- 05_squares/squares.c
+++ 05_squares/squares.c
@@ -49,12 +49,12 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
     {
       for (int x=0; x<=w; x++)
 	{
-	  if ((isInRange(x, x_offset, size2) && isAtBorder(y,y_offset,size2-1)) || (isInRange(y,y_offset,size2) && (isAtBorrder(x,x_offset,size2-1))))
+	  if ((isInRange(x, x_offset, size2) && isAtBorder(y,y_offset,size2-1)) || (isInRange(y,y_offset,size2) && (isAtBorder(x,x_offset,size2-1))))
 	    {
 	      printf("*");
 	    }
 	  else{
-	    if ((x<size1 && (y==0 || y==size1-1)) || ((y<size1) && (x==0 ||x=size1-1)))
+	    if ((x<size1 && (y==0 || (y==size1-1))) || ((y<size1) && (x==0 ||(x=size1-1))))
 	      {
 		printf("#");
 	      }
